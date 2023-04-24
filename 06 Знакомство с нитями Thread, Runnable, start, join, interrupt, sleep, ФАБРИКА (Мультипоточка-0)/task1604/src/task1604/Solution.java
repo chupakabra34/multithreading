e8e6e2e6dca8f87434a918fcdaf462ad6f237370
Sergei Prekrasnov <sap685@gmail.com>
2023-04-24 22:21:52 +0300

    Home -> Work 24-04-2023

--- 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1604/src/task1604/Solution.java
+++ 06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1604/src/task1604/Solution.java
@@ -25,4 +25,11 @@ public class Solution {
             System.out.println(element);
         }
     }
+    public static class SpecialThread implements Runnable{
+
+        @Override
+        public void run() {
+            ///// возможно тут надо дописать ещё что-то
+        }
+    }
 }
